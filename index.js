f7ae18d8bd59b25ce5dee5e9a4e68047f816c87e
Ashley McDermott <jamcdermott98@gmail.com>
2021-02-17 11:59:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "Hello, ";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, " + specialGuest + "!";
+var greetSpecialGuest = "Hello, everybody!" + specialGuest + "!";
 
 var topic = "It is a pleasure to meet you";
 
